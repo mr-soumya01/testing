4bd931e15d74021bf07ad8fb8e215beafd6ee509
soumya <soumyaranjansahu20037@gmail.com>
2022-12-27 14:41:20 +0530

    Testing

---
+++ src/__tests__/greet-test.tsx
@@ -0,0 +1,22 @@
+/**
+ *Greet should render the text hello and if a name is passed into the component
+ *It should render hello followed by the name
+ */
+
+import { render, screen } from "@testing-library/react";
+import { Greet } from "../components/greet";
+
+describe("Greet", () => {
+  test("Greet renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText("Hello");
+    expect(textElement).toBeInTheDocument();
+  });
+});
+describe("Nested", () => {
+  test("Greet renders with a name", () => {
+    render(<Greet name="Soumya" />);
+    const textElement = screen.getByText("Hello Soumya");
+    expect(textElement).toBeInTheDocument();
+  });
+});
